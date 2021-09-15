bfd9c3238cd6053eafd2f4c65a27ce3e0f902e01
Hubertformin <Rosemary.com98>
2021-09-15 17:57:03 +0100

    Disable REST SERVER

--- app.js
+++ app.js
@@ -27,52 +27,52 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //Connect to database
-const MONGODB_URI = "mongodb+srv://nudle:nudle@cluster0.aahux.mongodb.net/coreDB?retryWrites=true&w=majority";
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-mongoose.connection.on("connected", () => {
-  console.log("Connected to database!");
-});
+// const MONGODB_URI = "mongodb+srv://nudle:nudle@cluster0.aahux.mongodb.net/coreDB?retryWrites=true&w=majority";
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// mongoose.connection.on("connected", () => {
+//   console.log("Connected to database!");
+// });
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/inventory', inventoryRouter);
-app.use('/inventory-categories', inventoryCategoryRouter);
-app.use('/orders',ordersRouter);
-app.use('/reports',reportsRouter);
-app.use('/search', searchRouter);
+// // Routes
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/inventory', inventoryRouter);
+// app.use('/inventory-categories', inventoryCategoryRouter);
+// app.use('/orders',ordersRouter);
+// app.use('/reports',reportsRouter);
+// app.use('/search', searchRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
-// module.exports = app;
-http.listen(process.env.PORT || 5000, function () {
-  const host = http.address().address;
-  const port = process.env.PORT || 5000; //http.address().port;
-  console.log(`App listening on port ${port}`);
-});
+// // module.exports = app;
+// http.listen(process.env.PORT || 5000, function () {
+//   const host = http.address().address;
+//   const port = process.env.PORT || 5000; //http.address().port;
+//   console.log(`App listening on port ${port}`);
+// });
 
 // sync server
 let syncServer = new SyncServer(2100);
