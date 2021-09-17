aac7e8fc74bddd5d7a8043347d5260da1871a6f4
Hubertformin <Rosemary.com98>
2021-09-17 11:09:19 +0100

    Redirect socket server to default port

--- app.js
+++ app.js
@@ -1,80 +1,81 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require("mongoose");
-const cors = require('cors');
+// const createError = require('http-errors');
+// const express = require('express');
+// const path = require('path');
+// const cookieParser = require('cookie-parser');
+// const logger = require('morgan');
+// const mongoose = require("mongoose");
+// const cors = require('cors');
 const SyncServer = require('./socket/syncServer.js');
 
 // mongoose.set("useFindAndModify", false);
 // mongoose.set("useCreateIndex", true);
 
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const inventoryRouter = require('./routes/inventory');
-const ordersRouter = require('./routes/orders');
-const reportsRouter = require('./routes/reports');
-const inventoryCategoryRouter = require('./routes/inventoryCategory');
-const searchRouter = require('./routes/search');
+// const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
+// const inventoryRouter = require('./routes/inventory');
+// const ordersRouter = require('./routes/orders');
+// const reportsRouter = require('./routes/reports');
+// const inventoryCategoryRouter = require('./routes/inventoryCategory');
+// const searchRouter = require('./routes/search');
 
-const app = express();
-app.use(cors());
+// const app = express();
+// app.use(cors());
 
-const http = require("http").createServer(app);
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// const http = require("http").createServer(app);
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
-// Connect to database
-const MONGODB_URI = "mongodb+srv://nudle:nudle@cluster0.aahux.mongodb.net/coreDB?retryWrites=true&w=majority";
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  // useFindAndModify: false,
-  // useCreateIndex: true
-}).catch(err => console.log(err));
-mongoose.connection.on("connected", () => {
-  console.log("[MONGOOSE]: onnected to database!");
-});
+// // Connect to database
+// const MONGODB_URI = "mongodb+srv://nudle:nudle@cluster0.aahux.mongodb.net/coreDB?retryWrites=true&w=majority";
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   // useFindAndModify: false,
+//   // useCreateIndex: true
+// }).catch(err => console.log(err));
+// mongoose.connection.on("connected", () => {
+//   console.log("[MONGOOSE]: onnected to database!");
+// });
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/inventory', inventoryRouter);
-app.use('/inventory-categories', inventoryCategoryRouter);
-app.use('/orders',ordersRouter);
-app.use('/reports',reportsRouter);
-app.use('/search', searchRouter);
+// // Routes
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/inventory', inventoryRouter);
+// app.use('/inventory-categories', inventoryCategoryRouter);
+// app.use('/orders',ordersRouter);
+// app.use('/reports',reportsRouter);
+// app.use('/search', searchRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
-// module.exports = app;
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+// // module.exports = app;
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 // sync server
-let syncServer = new SyncServer(2100);
+const PORT = process.env.PORT || 5000;
+let syncServer = new SyncServer(PORT);
 syncServer.init()
 .then(() => {
   // start....
